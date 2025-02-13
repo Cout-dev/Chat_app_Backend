31b7420c0b7119d4f42c12c110f871fbf580a652
Cout_dev <shubhankarrai007@gmail.com>
2025-02-14 01:25:08 +0530

    sever

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,30 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https://chat-application-backend-2yuj.onrender.com"],
+          "img-src": ["'self'", "data:", "blob:"],
+          "media-src": ["'self'", "data:", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ["http://localhost:5173", "https://your-frontend-url.com"],  // Add allowed origins
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+      headers: "*",
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
